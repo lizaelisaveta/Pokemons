ed41e8b6716a484daecc71ac21894b3fea2abb81
LizaElizaveta <105479016+lizaelisaveta@users.noreply.github.com>
2023-09-30 00:29:12 +0500

    Add beautiful interface, add search

--- project/aplication/urls.py
+++ project/aplication/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list_names, name='list_names'),
+    path('search/', views.search_results, name='search_results')
+
 ]
--- project/aplication/views.py
+++ project/aplication/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .models import Pokemon
+import requests
 
 def list_names(request):
-    return render(request, 'aplication/list_names.html', {})
+    name_poke = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    name_poke_json = name_poke.json()
+    names = [result['name'] for result in name_poke_json['results']]
+
+    return render(request, 'aplication/list_names.html', {'names': names})
+
+
+def search_results(request):
+    query = request.GET.get('query')
+    name_poke = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    name_poke_json = name_poke.json()
+    names = [result['name'] for result in name_poke_json['results']]
+    matching_names = [name for name in names if name.lower().startswith(query.lower())]
+    
+    return render(request, 'aplication/list_names.html', {'names': matching_names})
