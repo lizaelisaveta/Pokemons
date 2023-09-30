71469c2ab68dac3bee3f8fcffc8d09d1b49fbc16
LizaElizaveta <105479016+lizaelisaveta@users.noreply.github.com>
2023-09-30 11:32:03 +0500

    add pagination

--- project/aplication/views.py
+++ project/aplication/views.py
@@ -11,10 +11,13 @@ def list_names(request):
     name_poke_json = name_poke.json()
     names = [result['name'] for result in name_poke_json['results']]
     
+    count_pages = 20
+    paginator = Paginator(names, count_pages)
     
-    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    return render(request, 'aplication/list_names.html', {'names': names, 'counts': counts})
+    return render(request, 'aplication/list_names.html', {'names': page_obj.object_list, 'counts': counts, 'paginator':paginator, 'page_obj': page_obj})
 
 
 def search_results(request):
