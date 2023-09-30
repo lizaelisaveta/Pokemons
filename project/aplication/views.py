33455b25b1348c5a5481f36ffeca179a5faa5be0
LizaElizaveta <105479016+lizaelisaveta@users.noreply.github.com>
2023-09-30 09:08:37 +0500

    Number of requests corrected

--- project/aplication/views.py
+++ project/aplication/views.py
@@ -3,18 +3,24 @@ from .models import Pokemon
 import requests
 
 def list_names(request):
-    name_poke = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    count = requests.get('https://pokeapi.co/api/v2/pokemon?limit=1&offset=0')
+    count_poke_json = count.json()
+    counts = count_poke_json['count']
+    name_poke = requests.get('https://pokeapi.co/api/v2/pokemon?limit=' + str(counts) + '&offset=0')
     name_poke_json = name_poke.json()
     names = [result['name'] for result in name_poke_json['results']]
 
-    return render(request, 'aplication/list_names.html', {'names': names})
+    return render(request, 'aplication/list_names.html', {'names': names, 'counts': counts})
 
 
 def search_results(request):
     query = request.GET.get('query')
-    name_poke = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    count = requests.get('https://pokeapi.co/api/v2/pokemon?limit=1&offset=0')
+    count_poke_json = count.json()
+    counts = count_poke_json['count']
+    name_poke = requests.get('https://pokeapi.co/api/v2/pokemon?limit=' + str(counts) + '&offset=0')
     name_poke_json = name_poke.json()
     names = [result['name'] for result in name_poke_json['results']]
     matching_names = [name for name in names if name.lower().startswith(query.lower())]
     
-    return render(request, 'aplication/list_names.html', {'names': matching_names})
+    return render(request, 'aplication/list_names.html', {'names': matching_names, 'counts': len(matching_names)})
