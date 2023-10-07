528971a2a34be64f708ff0b9b755a4b0c3007f99
LizaElizaveta <105479016+lizaelisaveta@users.noreply.github.com>
2023-10-07 09:59:09 +0500

    Попытки сделать вывод карточек

--- project/aplication/views.py
+++ project/aplication/views.py
@@ -1,7 +1,18 @@
+from email.mime import image
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Pokemon
 import requests
+
+class Pokemon:
+ 
+    def __init__(self, name, image, url):
+        self.url = url
+        self.id = url.split('/')[-2]
+        self.name = name   
+        self.image = image
+        
+
 
 def list_names(request):
     query = ''
@@ -19,16 +30,21 @@ def list_names(request):
     name_poke = requests.get('https://pokeapi.co/api/v2/pokemon?limit=' + str(counts) + '&offset=0')
     name_poke_json = name_poke.json()
     names = [result['name'] for result in name_poke_json['results']]
-    
+    urls = [result['url'] for result in name_poke_json['results']]
     images = display_images(name_poke_json, counts)
     
-    count_pages = 20
-    paginator = Paginator(images, count_pages)
+    pokemon_data = [[name for name in names], [image for image in images], [url for url in urls]]
+    pokemon_list = [Pokemon(name, image, url) for name, image, url in zip(*pokemon_data)]
+    
+    
+    count_pages = 18
+    paginator = Paginator(pokemon_list, count_pages)
     
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+    
 
-    return render(request, 'aplication/list_names.html', {'names': page_obj.object_list, 'counts': counts, 'paginator': paginator, 'page_obj': page_obj, 'query': query, 'images': images})
+    return render(request, 'aplication/list_names.html', {'pokemon_list': page_obj.object_list, 'counts': counts, 'paginator': paginator, 'page_obj': page_obj, 'query': query})
 
 def search_results(request):
     query = ''
@@ -48,13 +64,19 @@ def search_results(request):
     names = [result['name'] for result in name_poke_json['results']]
     matching_names = [name for name in names if name.lower().startswith(query.lower())]
     
+    images = display_images(name_poke_json, counts)
+    
+    pokemon_data = [[name for name in matching_names] for image in images]
+    pokemon_list = [Pokemon(name, image, url) for name, image, url in pokemon_data]
+    
     count_pages = 20
-    paginator = Paginator(matching_names, count_pages)
+    paginator = Paginator(pokemon_list, count_pages)
+    
     
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
-    return render(request, 'aplication/list_names.html', {'names': page_obj.object_list, 'counts': len(matching_names), 'paginator':paginator, 'page_obj': page_obj, 'query': query})
+    return render(request, 'aplication/list_names.html', {'pokemon_list': page_obj.object_list, 'counts': len(matching_names), 'paginator':paginator, 'page_obj': page_obj, 'query': query})
 
 
 def display_images(name_poke_json, counts):
