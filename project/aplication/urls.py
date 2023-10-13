a94a2b97861dd57907a505d7ad6e7fc04e0717c3
LizaElizaveta <105479016+lizaelisaveta@users.noreply.github.com>
2023-10-13 22:20:01 +0500

    fix bug with image in searh_string, add base info on faghts_page, add route fights

--- project/aplication/urls.py
+++ project/aplication/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.list_names, name='list_names'),
     path('search/', views.search_results, name='search_results'),
     path('detail/<str:name>/', views.details, name='details'),
-    path('fight/', views.fights, name='fights'),
+    path('fight/<str:name>/', views.fights, name='fights'),
+    path('fastfight/', views.fights_fast, name='fights_fast'),
+    path('simplefight/', views.fights_simple, name='fights_simple'),
 ]
