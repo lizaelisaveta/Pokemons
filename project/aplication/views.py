d0301c68b66c43499ef43f5732ab064425f834b9
LizaElizaveta <elissa_egorova_495@mail.ru>
2023-11-10 02:05:39 +0500

    start docker

--- project/aplication/views.py
+++ project/aplication/views.py
@@ -14,8 +14,8 @@ from django.conf import settings
 import redis
 
 
-redis_client = redis.StrictRedis(host=settings.REDIS_HOST,
-                                  port=settings.REDIS_PORT, db=0)
+redis_client = redis.StrictRedis(host='otrpo-redis',
+                                  port=6379, db=0)
 
 
 def get_pokemons(page):
